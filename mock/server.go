899aa851e511a2c1dc4d2a159b3c741da6fb26b9
r_fukuda <r_fukuda@RN0830.local>
2019-02-18 16:12:03 +0900

    curl localhost:8080

--- mock/server.go
+++ mock/server.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
 	"github.com/google/uuid"
+	"net/http"
 )
 
+type DspResponse struct {
+	RequestID string `json:"request_id"`
+	URL       string `json:"url"`
+	Price     string `json:"price"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World")
+	id, _ := uuid.NewUUID()
+
+	dspjson := DspResponse{}
+	dspjson.RequestID = id.String()
+	dspjson.URL = "http://hoge.com"
+	dspjson.Price = "50"
+	out, _ := json.Marshal(dspjson)
+	fmt.Fprintf(w, string(out))
+
 }
 
 func main() {
-	fmt.Println(uuid.NewUUID())
 	fmt.Println("server start")
 	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
 	http.ListenAndServe(":8080", nil)
