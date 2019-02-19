91daf2607b4ec50559a10570972f141a3d32e550
r_fukuda <r_fukuda@RN0830.local>
2019-02-19 13:00:35 +0900

    go fmt mock/main.go

--- mock/main.go
+++ mock/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"strconv"
-	"math/rand"
-	"io/ioutil"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -17,7 +17,6 @@ type DspResponse struct {
 	Price     string `json:"price"`
 }
 
-
 // DspRequest is convert to json
 type DspRequest struct {
 	SspName     string `json:"ssp_name"`
@@ -25,28 +24,26 @@ type DspRequest struct {
 	RequestID   string `json:"request_id"`
 }
 
-
 func handler(w http.ResponseWriter, r *http.Request) {
 
 	data, _ := ioutil.ReadAll(r.Body)
 	sspReq := DspRequest{}
-	json.Unmarshal(data,&sspReq)
+	json.Unmarshal(data, &sspReq)
 	fmt.Println(sspReq)
 	dspjson := DspResponse{}
 	dspjson.RequestID = sspReq.RequestID
 	dspjson.URL = "http://hoge.com"
 	dspjson.Price = randTOstring()
+
+	fmt.Println(dspjson)
 	out, _ := json.Marshal(dspjson)
-	
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, string(out))
 }
 
-
 func randTOstring() string {
 	rand.Seed(time.Now().UnixNano())
 	num := rand.Intn(150)
-	fmt.Println(strconv.Itoa(num))
 	return strconv.Itoa(num)
 }
 
@@ -54,7 +51,6 @@ func now() string {
 	t := time.Now()
 	return t.String()
 }
-
 
 func main() {
 	fmt.Println("server start")
