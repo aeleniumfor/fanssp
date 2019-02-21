666ed595fab2de6aa3c935314f95792c989c9297
r_fukuda <r_fukuda@RN0830.local>
2019-02-21 17:21:27 +0900

    tes

--- ssp/main.go
+++ ssp/main.go
@@ -119,11 +119,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		auction = append(auction, data)
 	} else if len(auction) == 1 {
 		// レスポンスが1つの場合
-		win := WinNotice{
-			RequestID: ids,
-			Price:     1,
-		}
-		winrequest(win, HostArray[0])
+		// win := WinNotice{
+		// 	RequestID: ids,
+		// 	Price:     1,
+		// }
+		//winrequest(win, HostArray[0])
 
 	} else {
 		// ソートするやつ 数値以外が来たら終わる
